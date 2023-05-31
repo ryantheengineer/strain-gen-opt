13d4b9c6ea7e6b37cfbb9a3d3a8451b94086ebfb
ryantheengineer <ryan.larson.wa@gmail.com>
2023-05-30 22:57:10 -0600

    Update constraints_prototyping.py
    Find grid of circles that fit within UUT boundaries

--- constraints_prototyping.py
+++ constraints_prototyping.py
@@ -6,9 +6,11 @@ This is a temporary script file.
 """
 
 from shapely.geometry import Point, Polygon
+from shapely.prepared import prep
 import matplotlib.pyplot as plt
-import pandas as pd
+# import pandas as pd
 import random
+import numpy as np
 
 def UUT_square(maxbnd):
     coords = ((0.,0.),(0.,maxbnd),(maxbnd,maxbnd),(maxbnd,0.),(0.,0.))
@@ -32,7 +34,9 @@ def random_circle(radius,maxbnd):
     circle = Point(x, y).buffer(radius)
     return circle
 
-
+def place_circle(x,y,radius):
+    circle = Point(x, y).buffer(radius)
+    return circle
 
 
 if __name__ == "__main__":
@@ -41,9 +45,12 @@ if __name__ == "__main__":
     # avoided by circles. Plot these in blue. Create a series of random circles
     # to represent pins that do not conflict with any features in the UUT.
     maxbnd = 100.
-    n_rectangles = 40
+    n_rectangles = 20
     min_rect = 2.
     max_rect = 20.
+    n_circles = 10
+    radius = 2.
+    resolution = 5.
     
     UUT_poly = UUT_square(maxbnd)
     
@@ -67,6 +74,42 @@ if __name__ == "__main__":
                 
     for interior in interiors:
         UUT_poly = UUT_poly.difference(interior)
+        
+    
+    # Place circles on grid within UUT
+    xmin, ymin, xmax, ymax = UUT_poly.bounds
+    
+    prep_UUT_poly = prep(UUT_poly)
+    
+    circles = []
+    for x in np.arange(xmin, xmax, resolution):
+        for y in np.arange(ymin, ymax, resolution):
+            c = place_circle(x, y, radius)
+            circles.append(c)
+    
+    # Validate that each circle falls inside shape
+    valid_circles = []
+    valid_circles.extend(filter(UUT_poly.contains, circles))
+    
+    # # Randomly place circles
+    # circles = []
+    # for i in range(n_circles):
+    #     while True:
+    #         p = random_circle(radius, maxbnd)
+    #         conflict = False
+    #         for circle in circles:
+    #             if circle.intersects(p):
+    #                 conflict = True
+    #                 break
+    #             elif UUT_poly.contains(circle) == False:
+    #                 conflict = True
+    #                 break
+    #         if conflict == False:
+    #             circles.append(p)
+    #             break
+    #         else:
+    #             continue
+    
     
     fig, ax = plt.subplots()
     
@@ -77,7 +120,11 @@ if __name__ == "__main__":
         ax.plot(xi, yi, color="blue")
      
     ax.plot(xe, ye, color="blue")
-    # ax.axis([0, 100, 0, 100])
+    
+    # Plot circles
+    for circle in valid_circles:
+        xe, ye = circle.exterior.xy
+        ax.plot(xe, ye, color="red")
     plt.show()
     
      
