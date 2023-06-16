42114eb5dc232d826ae23aff2bbb987d31fc4a9e
ryantheengineer <ryan.larson.wa@gmail.com>
2023-06-16 00:07:34 -0600

    Update input_xml_parsing.py

--- input_xml_parsing.py
+++ input_xml_parsing.py
@@ -4,7 +4,6 @@ Created on Thu Jun 15 23:20:26 2023
 
 @author: Ryan Larson
 """
-
 import xmltodict
 
 
@@ -30,6 +29,42 @@ if __name__ == "__main__":
     yMax_panel = fixture_dict["struct"][0]["yMax"]
     yMin_panel = fixture_dict["struct"][0]["yMin"]
     
+    for panel in panels:
+        if 'pBoards' in panel.values():
+            pBoards_vertices = panel['region']['vertex']
+            break
+        else:
+            pBoards_vertices = None
+    
     # Get relevant information about plates
     plates = fixture_dict["struct"][1]["polyshape"]
     plate_types = [plate["@identifier"] for plate in plates]
+
+
+
+
+
+
+
+
+# import pandas as pd
+# import xml.etree.ElementTree as ET
+
+# # Load the XML file
+# tree = ET.parse('FEA_Example2.xml')
+
+# # Get the root element
+# root = tree.getroot()
+
+# # Create a DataFrame
+# df = pd.DataFrame()
+
+# # Iterate over the child elements of the root element
+# for child in root:
+#     # Add the child element's text to the DataFrame
+#     df[child.tag] = child.text
+
+# # Print the DataFrame
+# print(df)
+
+
