684c4b2e96551617b6cf0f632f1cb716282c2261
ryantheengineer <ryan.larson.wa@gmail.com>
2023-07-19 23:21:21 -0600

    Successful plate plots

--- constraints.py
+++ constraints.py
@@ -23,6 +23,7 @@ import xml.etree.ElementTree as ET
 import pickle
 import pathlib
 import matplotlib.pyplot as plt
+import runFEA
 
 # Get the root of the XML tree that will be used for all other input parsing
 def get_XML_tree(initialdir):
@@ -104,68 +105,6 @@ def get_fixture_geometry(root, identifier):
     
     return region_polys, numRegions, numHoles
 
-# # pBoards
-# def get_pBoards(root):
-#     polyshapes = root.findall('.//polyshape')
-#     pBoards = None
-#     region_polys = None
-#     hole_polys = None
-#     for polyshape in polyshapes:
-#         if polyshape.attrib.get("identifier") == "pBoards":
-#             pBoards = polyshape
-#             break
-    
-#     def verts_to_poly(polys, elem):
-#         vertices_elem = elem.findall('.//vertex')
-#         vertices = [vertex.text.split("|") for vertex in vertices_elem]
-#         polys.append(Polygon(vertices))
-        
-        
-#     if pBoards:
-#         numRegions = int(pBoards.find('.//numRegions').text)
-#         numHoles = int(pBoards.find('.//numHoles').text)
-#         if numRegions:
-#             # Create the Polygons of each region
-#             regions = pBoards.findall('.//region')
-#             region_polys = []
-#             for region in regions:
-#                 verts_to_poly(region_polys, region)
-#                 # vertices_elem = region.findall('.//vertex')
-#                 # vertices = [vertex.text.split("|") for vertex in vertices_elem]
-#                 # region_polys.append(Polygon(vertices))
-#         if numHoles:
-#             # Create the Polygons of each hole
-#             holes = pBoards.findall('.//hole')
-#             hole_polys = []
-#             for hole in holes:
-#                 verts_to_poly(hole_polys, hole)
-#                 # vertices_elem = hole.findall('.//vertex')
-#                 # vertices = [vertex.text.split("|") for vertex in vertices_elem]
-#                 # hole_polys.append(Polygon(vertices))
-#     else:
-#         region_polys = None
-#         numRegions = None
-#         numHoles = None
-        
-#     # If there are holes, determine which region they belong to and subtract
-#     # them to make a list of complete polygons
-#     # NOTE: This assumes that all of the polygons produced by get_pBoards are
-#     # unique and will not overlap each other.
-#     if numHoles:
-#         holes_grouped = [[] for poly in region_polys]
-#         for hole_poly in hole_polys:
-#             for i,region_poly in enumerate(region_polys):
-#                 if region_poly.contains(hole_poly):
-#                     holes_grouped[i].append(hole_poly)
-#                     continue
-                
-#         # Subtract the holes from their regions
-#         for i in range(len(region_polys)):
-#             for hole_poly in holes_grouped[i]:
-#                 region_polys[i] = region_polys[i].difference(hole_poly)
-    
-#     return region_polys, numRegions, numHoles
-
 
 def get_region_shifts(region_polys):
     polys_copy = region_polys.copy()
@@ -190,8 +129,10 @@ def get_region_shifts(region_polys):
                 xshifts.append(xshift)
                 yshifts.append(xshift)
                 rotations.append(None)
+                
+    dfshifts = pd.DataFrame({"xshift":xshifts, "yshift":yshifts, "rotation":rotations})
     
-    return xshifts, yshifts, rotations
+    return dfshifts
                 
             
 def check_shifted(refpoly, poly):
@@ -231,14 +172,51 @@ def plot_poly_list_w_holes(poly_list, fig, ax, color, linestyle, label):
     for poly in poly_list:
         xe, ye = poly.exterior.xy
         if first == True:
-            ax.plot(xe, ye, color=color, label=label, linestyle=linestyle)
+            ax.plot(xe, ye, color=color, label=label, linestyle=linestyle, linewidth=0.5)
             first = False
         else:
-            ax.plot(xe, ye, color=color, linestyle=linestyle)  
+            ax.plot(xe, ye, color=color, linestyle=linestyle, linewidth=0.5)
         for inner in poly.interiors:
             xi, yi = zip(*inner.coords[:])
-            ax.plot(xi, yi, color=color, linestyle=linestyle)
-
+            ax.plot(xi, yi, color=color, linestyle=linestyle, linewidth=0.5)
+            
+def plot_probes_guidepins(df, fig, ax, linestyle, identifier):
+    first = True
+    for row in range(len(df)):
+        x = df.loc[row,"x"]
+        y = df.loc[row,"y"]
+        radius = df.loc[row,"diameter"]/2
+        circle = Point(x, y).buffer(radius)
+        xe, ye = circle.exterior.xy
+        color = tuple(df.loc[row,"color"])
+        if first == True:
+            ax.plot(xe, ye, color=color, label=identifier, linestyle=linestyle, linewidth=0.5)
+            first = False
+        else:
+            ax.plot(xe, ye, color=color, linestyle=linestyle, linewidth=0.5)
+            
+def plot_pressurerods_standoffs(df, fig, ax, linestyle, identifier):
+    if identifier == "PressureRods":
+        radius = 0.0625/2
+    elif identifier == "Standoffs":
+        radius = 0.15/2
+    else:
+        raise ValueError("Invalid identifier fed to plot_pressurerods_standoffs")
+        
+    first = True
+    for row in range(len(df)):
+        x = df.loc[row,"x"]
+        y = df.loc[row,"y"]
+        circle = Point(x, y).buffer(radius)
+        xe, ye = circle.exterior.xy
+        color = tuple(df.loc[row,"color"])
+        if first == True:
+            ax.plot(xe, ye, color=color, label=identifier, linestyle=linestyle, linewidth=0.5)
+            first = False
+        else:
+            ax.plot(xe, ye, color=color, linestyle=linestyle, linewidth=0.5)
+    
+    
 
 def get_region_list_centroids(region_polys):
     region_centroids = [poly.centroid for poly in region_polys]
@@ -246,17 +224,69 @@ def get_region_list_centroids(region_polys):
 
 
 
-def get_table_rows(root, identifier):
+# def get_table_rows(root, identifier):
+#     tables = root.findall('.//table')
+#     rows = None
+#     for table in tables:
+#         if table.attrib.get("identifier") == identifier:
+#             # Extract rows
+#             rows = []
+#             for row in table.findall('.//row'):
+#                 rows.append(row.text.split('|'))
+#     return rows
+
+def get_point_geometry(root, identifier):
     tables = root.findall('.//table')
+    column_names = None
+    types = None
     rows = None
+    df_output = None
     for table in tables:
         if table.attrib.get("identifier") == identifier:
-            # Extract rows
+            column_names = []
+            for col in table.findall('.//col'):
+                column_names.append(col.text)
+            
+            types = []
+            for typ in table.findall('.//type'):
+                types.append(typ.text)
+            
             rows = []
             for row in table.findall('.//row'):
                 rows.append(row.text.split('|'))
-    return rows
-                        
+            
+            if len(column_names) != len(types) or len(column_names) != len(rows[0]):
+                raise ValueError(f"Mismatch between number of columns, types, or row elements for {identifier}")
+            
+            columns = []
+            for i in range(len(column_names)):
+                columns.append([])
+                for j in range(len(rows)):
+                    columns[i].append(interpret_type_description(types[i],rows[j][i]))
+                    
+            output_dict = {column_names[i]:columns[i] for i in range(len(column_names))}
+            df_output = pd.DataFrame(output_dict)
+             
+    return df_output
+            
+
+def interpret_type_description(type_str, elem):
+    if type_str == "double":
+        if elem[0]=='[' and elem[-1]==']':
+            res = elem.strip('][').split(',')
+            reslist = [float(x) for x in res]
+            return reslist
+        else:
+            return float(elem)
+    elif type_str == "string":
+        return elem
+    elif type_str == "logical":
+        if elem.lower() == "false":
+            return False
+        else:
+            return True
+    
+
 
 
 
@@ -274,23 +304,23 @@ if __name__ == "__main__":
     pBoards, _, _ = get_fixture_geometry(root, "pBoards")
     pOutline, _, _ = get_fixture_geometry(root, "pOutline")
     pShape, _, _ = get_fixture_geometry(root, "pShape")
-    pTopComponents, _, _ = get_fixture_geometry(root, "pTopComponents")
-    pBottomComponents, _, _ = get_fixture_geometry(root, "pBottomComponents")
-    fig1, ax1 = plt.subplots(figsize=(10,8),dpi=300)
-    ax1.set_aspect('equal')
-    labels = []
-    line = "-"
-    if pBoards:
-        plot_poly_list_w_holes(pBoards, fig1, ax1, "blue", line, "pBoards")
-    if pOutline:
-        plot_poly_list_w_holes(pOutline, fig1, ax1, "green", line, "pOutline")
-    if pShape:
-        plot_poly_list_w_holes(pShape, fig1, ax1, "black", line, "pShape")
-    if pTopComponents:
-        plot_poly_list_w_holes(pTopComponents, fig1, ax1, "purple", line, "pTopComponents")
-    if pBottomComponents:
-        plot_poly_list_w_holes(pBottomComponents, fig1, ax1, "orange", line, "pBottomComponents")
-    ax1.legend()
+    pComponentsTop, _, _ = get_fixture_geometry(root, "pComponentsTop")
+    pComponentsBot, _, _ = get_fixture_geometry(root, "pComponentsBot")
+    # fig1, ax1 = plt.subplots(figsize=(10,8),dpi=300)
+    # ax1.set_aspect('equal')
+    # labels = []
+    # line = "-"
+    # if pBoards:
+    #     plot_poly_list_w_holes(pBoards, fig1, ax1, "blue", line, "pBoards")
+    # if pOutline:
+    #     plot_poly_list_w_holes(pOutline, fig1, ax1, "green", line, "pOutline")
+    # if pShape:
+    #     plot_poly_list_w_holes(pShape, fig1, ax1, "black", line, "pShape")
+    # if pComponentsTop:
+    #     plot_poly_list_w_holes(pComponentsTop, fig1, ax1, "purple", line, "pComponentsTop")
+    # if pComponentsBot:
+    #     plot_poly_list_w_holes(pComponentsBot, fig1, ax1, "orange", line, "pComponentsBot")
+    # ax1.legend()
     
     # Plates
     Pressure, _, _ = get_fixture_geometry(root, "Pressure")
@@ -298,37 +328,110 @@ if __name__ == "__main__":
     Stripper, _, _ = get_fixture_geometry(root, "Stripper")
     Probe, _, _ = get_fixture_geometry(root, "Probe")
     Countersink, _, _ = get_fixture_geometry(root, "Countersink")
-    # fig2, ax2 = plt.subplots(figsize=(10,8),dpi=300)
-    # ax2.set_aspect('equal')
-    labels = []
-    line = ":"
-    if Pressure:
-        plot_poly_list_w_holes(Pressure, fig1, ax1, "blue", line, "Pressure")
+    df_Probes = get_point_geometry(root, "Probes")
+    df_GuidePins = get_point_geometry(root, "GuidePins")
+    df_PressureRods = get_point_geometry(root, "PressureRods")
+    df_Standoffs = get_point_geometry(root, "Standoffs")
+    
+    # Top Plate/Pressure Plate (doesn't hold pressure rods if I_plate exists)
     if I_Plate:
-        plot_poly_list_w_holes(I_Plate, fig1, ax1, "green", line, "I_Plate")
+        fig1, ax1 = plt.subplots(figsize=(10,8),dpi=500)
+        line="-"
+        plot_poly_list_w_holes(I_Plate, fig1, ax1, "blue", line, "I_Plate")
+        plot_poly_list_w_holes(Pressure, fig1, ax1, "red", line, "Pressure")
+        if pBoards:
+            plot_poly_list_w_holes(pBoards, fig1, ax1, "black", line, "pBoards")
+        if pComponentsTop:
+            plot_poly_list_w_holes(pComponentsTop, fig1, ax1, "purple", line, "pComponentsTop")
+        if df_PressureRods is not None:
+            plot_pressurerods_standoffs(df_PressureRods, fig1, ax1, line, "PressureRods")
+        ax1.set_title("Pressure and Intermediate Plates")
+        ax1.legend()
+    else:
+        print("\nExample has no intermediate plate\n")
+        fig1, ax1 = plt.subplots(figsize=(10,8),dpi=500)
+        line="-"
+        plot_poly_list_w_holes(Pressure, fig1, ax1, "blue", line, "Pressure")
+        if pBoards:
+            plot_poly_list_w_holes(pBoards, fig1, ax1, "black", line, "pBoards")
+        if pComponentsTop:
+            plot_poly_list_w_holes(pComponentsTop, fig1, ax1, "purple", line, "pComponentsTop")
+        if df_PressureRods is not None:
+            plot_pressurerods_standoffs(df_PressureRods, fig1, ax1, line, "PressureRods")
+        ax1.set_title("Pressure Plate")
+        ax1.legend()
+        
+    # Probe Protector Plate (Stripper Plate)
     if Stripper:
-        plot_poly_list_w_holes(Stripper, fig1, ax1, "black", line, "Stripper")
-    if Probe:
-        plot_poly_list_w_holes(Probe, fig1, ax1, "purple", line, "Probe")
-    if Countersink:
-        plot_poly_list_w_holes(Countersink, fig1, ax1, "orange", line, "Countersink")
-    ax1.legend()
+        fig2, ax2 = plt.subplots(figsize=(10,8),dpi=500)
+        line="-"
+        plot_poly_list_w_holes(Stripper, fig2, ax2, "blue", line, "Stripper")
+        plot_poly_list_w_holes(Probe, fig2, ax2, "red", line, "Probe")
+        if pBoards:
+            plot_poly_list_w_holes(pBoards, fig2, ax2, "black", line, "pBoards")
+        if pComponentsBot:
+            plot_poly_list_w_holes(pComponentsBot, fig2, ax2, "purple", line, "pComponentsBot")
+        if df_Probes is not None:
+            plot_probes_guidepins(df_Probes, fig2, ax2, line, "Probes")
+        if df_GuidePins is not None:
+            plot_probes_guidepins(df_GuidePins, fig2, ax2, line, "GuidePins")
+        if df_Standoffs is not None:
+            plot_pressurerods_standoffs(df_Standoffs, fig2, ax2, line, "Standoffs")
+        ax2.set_title("Probe and Probe Protector (Stripper)Plates")
+        ax2.legend()
+    else:
+        print("\nExample has no probe protector plate (stripper plate)\n")
+        fig2, ax2 = plt.subplots(figsize=(10,8),dpi=500)
+        line="-"
+        plot_poly_list_w_holes(Probe, fig2, ax2, "red", line, "Probe")
+        if pBoards:
+            plot_poly_list_w_holes(pBoards, fig2, ax2, "black", line, "pBoards")
+        if pComponentsBot:
+            plot_poly_list_w_holes(pComponentsBot, fig2, ax2, "purple", line, "pComponentsBot")
+        if df_Probes is not None:
+            plot_probes_guidepins(df_Probes, fig2, ax2, line, "Probes")
+        if df_GuidePins is not None:
+            plot_probes_guidepins(df_GuidePins, fig2, ax2, line, "GuidePins")
+        if df_Standoffs is not None:
+            plot_pressurerods_standoffs(df_Standoffs, fig2, ax2, line, "Standoffs")
+        ax2.set_title("Probe and Probe Protector (Stripper)Plates")
+        ax2.legend()
+        
+    plt.show()
+    
+    
+    # labels = []
+    # line = ":"
     # if Pressure:
-    #     plot_poly_list_w_holes(Pressure, fig2, ax2, "blue", line, "Pressure")
+    #     plot_poly_list_w_holes(Pressure, fig1, ax1, "blue", line, "Pressure")
     # if I_Plate:
-    #     plot_poly_list_w_holes(I_Plate, fig2, ax2, "green", line, "I_Plate")
+    #     plot_poly_list_w_holes(I_Plate, fig1, ax1, "green", line, "I_Plate")
     # if Stripper:
-    #     plot_poly_list_w_holes(Stripper, fig2, ax2, "black", line, "Stripper")
+    #     plot_poly_list_w_holes(Stripper, fig1, ax1, "black", line, "Stripper")
     # if Probe:
-    #     plot_poly_list_w_holes(Probe, fig2, ax2, "purple", line, "Probe")
+    #     plot_poly_list_w_holes(Probe, fig1, ax1, "purple", line, "Probe")
     # if Countersink:
-    #     plot_poly_list_w_holes(Countersink, fig2, ax2, "orange", line, "Countersink")
-    # ax2.legend()
+    #     plot_poly_list_w_holes(Countersink, fig1, ax1, "orange", line, "Countersink")
+    
+    
+    # line = "-"
+    # if df_Probes is not None:
+    #     plot_probes_guidepins(df_Probes, fig1, ax1, line, "Probes")
+    # if df_GuidePins is not None:
+    #     plot_probes_guidepins(df_GuidePins, fig1, ax1, line, "GuidePins")
+    # if df_PressureRods is not None:
+    #     plot_pressurerods_standoffs(df_PressureRods, fig1, ax1, line, "PressureRods")
+    # if df_Standoffs is not None:
+    #     plot_pressurerods_standoffs(df_Standoffs, fig1, ax1, line, "Standoffs")
+    
+    # ax1.legend()
     
     # plot_poly_list_w_holes(pBoards)
     # print(f"Regions:\t{numRegions}\nHoles:\t{numHoles}")
     
-    xshifts, yshifts, rotations = get_region_shifts(pBoards)
-    # xshifts, yshifts, rotations = get_region_shifts(pBoards)
-    df = pd.DataFrame({"xshift":xshifts, "yshift":yshifts, "rotation":rotations})
-    print(df)
+    
+    # 
+    
+    
+    dfshifts = get_region_shifts(pBoards)
+    
